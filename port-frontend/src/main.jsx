45aacd594862fbd87af26938e435c338ddf4b96a
Balaji-Rajini <balajitheprogrammer@gmail.com>
2026-06-16 14:38:02 +0530

    changes made with react-router-doms installation

--- port-frontend/src/main.jsx
+++ port-frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {Route} from 'react-router-dom'
+import {Route , BrowserRouter , Routes} from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
 
 
