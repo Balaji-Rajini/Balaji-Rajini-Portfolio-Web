25ca42dc864c47a292501c5e755b7bef7603f9cc
Balaji-Rajini <balajitheprogrammer@gmail.com>
2026-06-16 14:39:33 +0530

    changes made with react-router-doms installation

--- port-frontend/src/main.jsx
+++ port-frontend/src/main.jsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   
   <Routes>
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<App/>} />
      
      <Route path="/" element={<App />}></Route>
 
