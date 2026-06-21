c5711137341f432c66a73f1a4707a905ed10e2e0
Balaji-Rajini <balajitheprogrammer@gmail.com>
2026-06-21 10:46:47 +0530

    mailed with render link

--- port-backend/routes/contactRoute.js
+++ port-backend/routes/contactRoute.js
@@ -4,51 +4,77 @@ const nodemailer = require("nodemailer");
 
 const Contact = require("../models/Contact");
 
+console.log("Creating transporter...");
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+service: "gmail",
+auth: {
+user: process.env.EMAIL_USER,
+pass: process.env.EMAIL_PASS,
+},
+});
+
+transporter.verify((error, success) => {
+if (error) {
+console.log("VERIFY ERROR:", error);
+} else {
+console.log("MAIL SERVER READY");
+}
+});
+
+router.post("/", async (req, res) => {
+console.log("=================================");
+console.log("ROUTE HIT");
+console.log("Request Body:", req.body);
+
+try {
+const { name, email, enquiry } = req.body;
+
+console.log("Creating MongoDB document...");
+
+const contact = await Contact.create({
+  name,
+  email,
+  enquiry,
 });
 
 
-router.post("/", async (req, res) => {
-  console.log("Route Hitted");
+console.log("BEFORE EMAIL");
 
-  try {
-    const { name, email, enquiry } = req.body;
+const info = await transporter.sendMail({
+  from: process.env.EMAIL_USER,
+  to: "balajitheprogrammer@gmail.com",
+  subject: "New Portfolio Enquiry",
+  html: `
+    <h2>New Contact Form Submission</h2>
+    <p><strong>Name:</strong> ${name}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Enquiry:</strong> ${enquiry}</p>
+  `,
+});
 
-    console.log("Route Hitted")
-    const contact = await Contact.create({
-      name,
-      email,
-      enquiry,
-    });
+console.log("EMAIL SENT");
 
-    console.log("Saved:", contact);
-console.log("BEfore email")
-    // await transporter.sendMail({
-    //   from: process.env.EMAIL_USER,
-    //   to: "balajitheprogrammer@gmail.com",
-    //   subject: "New Portfolio Enquiry",
-    //   html: `
-    //     <h2>New Contact Form Submission</h2>
-    //     <p><strong>Name:</strong> ${name}</p>
-    //     <p><strong>Email:</strong> ${email}</p>
-    //     <p><strong>Enquiry:</strong> ${enquiry}</p>
-    //   `,
-    // });
+console.log("SENDING RESPONSE TO FRONTEND");
 
-    console.log("After Email ");
+res.status(201).json({
+  success: true,
+  contact,
+});
 
-    res.status(201).json(contact);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: error.message,
-    });
-  }
+console.log("RESPONSE SENT");
+console.log("=================================");
+
+} catch (error) {
+console.log("ERROR OCCURRED");
+console.log(error);
+
+res.status(500).json({
+  success: false,
+  message: error.message,
+});
+
+}
 });
 
 module.exports = router;
